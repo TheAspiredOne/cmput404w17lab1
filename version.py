ed623e71f709eda4335babf4de4666ad2e8fac56
Avery Tan <altan@cs.ualberta.ca>
2017-01-10 09:05:22 -0700

    modified version.py to download itself and print out its own contents

--- version.py
+++ version.py
@@ -4,6 +4,10 @@ import requests
 
 print requests.__version__
 
-response = requests.get("http://google.com")
+# response = requests.get("http://google.com")
+# print response.status_code
+# print response.text
+
+response = requests.get("https://raw.githubusercontent.com/TheAspiredOne/cmput404w17lab1/master/version.py")
 print response.status_code
 print response.text
